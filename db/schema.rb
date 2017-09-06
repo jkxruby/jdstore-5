37f1aed6c424c4d0ded110618b8f37d9e848cd28
jkxruby <jkxruby@gmail.com>
2017-09-06 11:45:01 +0800

    add aasm_state to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906022049) do
+ActiveRecord::Schema.define(version: 20170906032448) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -32,11 +32,13 @@ ActiveRecord::Schema.define(version: 20170906022049) do
     t.integer  "total",            default: 0
     t.string   "shipping_name"
     t.string   "shipping_address"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.string   "token"
     t.boolean  "is_paid",          default: false
     t.string   "payment_method"
+    t.string   "aasm_state",       default: "order_placed"
+    t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
   create_table "product_lists", force: :cascade do |t|
